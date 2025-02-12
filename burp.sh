104b7eeb693c19a69c6993d8c7535cf4e915c352
Mizi <mizi@mizi.ws>
2025-02-12 02:13:01 +0100

    Added burp 3 support

--- burp.sh
+++ burp.sh
@@ -4,23 +4,23 @@ CRON=false
 # if monitoring from burp server locally, this prevent's new clients to trigger unwanted "no backup" trigger
 #CLIENTDIR="/etc/burp/clientconfdir"
 
-# create new list of clients/backups if doesn't exist/is empty. might cause zabbix to timeout since querying burp can take a while.
-if [ ! -s /etc/zabbix/burp_list.txt ]; then
-        burp -a S | grep -v "burp status" | sed 's/^ *//' > /etc/zabbix/burp_list.txt
-fi
-
-burp_list=$(cat /etc/zabbix/burp_list.txt)
-
 function BurpCreateList {
 
 # create new list every 1hours, not every time this script runs. querying burp takes a while, so maybe move this task as cronjob if causing issues or incorrect data.
-if [ $(find /etc/zabbix -type f -name "burp_list.txt" -mmin +60) ] && [ ! -f /etc/zabbix/burp_list.lock ]; then
+if [ ! -s /etc/zabbix/burp_list.txt ] || [$(find /etc/zabbix -type f -name "burp_list.txt" -mmin +60) ] && [ ! -f /etc/zabbix/burp_list.lock ]; then
 	touch /etc/zabbix/burp_list.lock
-	burp -a S | grep -v "burp status" | sed 's/^ *//' > /etc/zabbix/burp_list.tmp && \
+	stdbuf -oL -- burp -a S | grep "^   " | sed 's/^ *//' > /etc/zabbix/burp_list.tmp && \
 	rm -f /etc/zabbix/burp_list.lock || { rm -f /etc/zabbix/burp_list.lock ; exit 1; }
 	mv -f /etc/zabbix/burp_list.tmp /etc/zabbix/burp_list.txt
 fi
 }
+
+# create new list of clients/backups if doesn't exist/is empty. might cause zabbix to timeout since querying burp can take a while.
+if [ ! -s /etc/zabbix/burp_list.txt ]; then
+	BurpCreateList
+fi
+
+burp_list=$(cat /etc/zabbix/burp_list.txt)
 
 function BurpDiscover {
 
@@ -52,13 +52,13 @@ function BurpClientCheck {
 client="$1"
 
 # if latest backup status is null/empty print negative number and exit
-if [[ $(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $5}') == "never" ]] || [[ $(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $5}') == "" ]]; then
+if [[ $(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $6}') == "never" ]] || [[ $(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $6}') == "" ]]; then
 	echo "0"
 	exit
 fi
 
 # else proceed with finding out the exact timestamp
-backup_data=$(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $6,$7}')
+backup_data=$(echo "$burp_list" | awk -v c="^$client$" '$1 ~ c' | awk '{print $6,$7,$8}')
 
 backup_time=$(date -d "$backup_data" +"%s")
 current_time=$(date +"%s")
